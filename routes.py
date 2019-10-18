ffdea2601fcf72a59819f783285450ce296ec42c
Brandon <b.mazey@nyu.edu>
2019-10-18 16:10:03 -0400

    adding some missing imports

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 from models import User
 from database import db
 
