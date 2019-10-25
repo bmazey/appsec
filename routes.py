8dd77206c3f4672161bb0c443fa377cad18d0dc3
Brandon <b.mazey@nyu.edu>
2019-10-25 16:54:58 -0400

    expanding templates with ids and statuses

--- routes.py
+++ routes.py
@@ -31,31 +31,35 @@ def index():
 @pages.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('pages.index'))
+        return redirect(url_for('pages.spellcheck'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data, phone=form.phone.data)
         user.set_password(form.phone.data, form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('pages.login'))
+        # flash('Congratulations, you are now a registered user!')
+        # return redirect(url_for('pages.login'))
+        # TODO - show failure element on failed registration?
+        return render_template('register.html', title='Register', form=form, registration="success")
     return render_template('register.html', title='Register', form=form)
 
 
 @pages.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('pages.index'))
+        return redirect(url_for('pages.spellcheck'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.phone.data, form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('pages.login'))
+            # flash('Invalid username or password')
+            # return redirect(url_for('pages.login'))
+            # TODO - 2factor custom failure code
+            return render_template('login.html', title='Sign In', form=form, authenticated="Incorrect")
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('pages.index'))
-    return render_template('login.html', title='Sign In', form=form)
+        return redirect(url_for('pages.spellcheck'))
+    return render_template('login.html', title='Sign In', form=form, authenticated="success")
 
 
 @pages.route('/logout')
