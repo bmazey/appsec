2c5185f738a6f0fd33fa5db1eb1408fb84235c41
Brandon <b.mazey@nyu.edu>
2019-10-25 11:44:21 -0400

    fixed redirect URLs

--- routes.py
+++ routes.py
@@ -28,7 +28,7 @@ def index():
 @pages.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('pages.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -36,26 +36,26 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('pages.login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @pages.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('pages.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('pages.login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('pages.index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
 @pages.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('pages.index'))
