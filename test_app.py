5c20206532a523c616273366c2543e13e04f24b8
Brandon <b.mazey@nyu.edu>
2019-10-29 16:34:02 -0400

    looking MUCH better - still having some db issues

--- test_app.py
+++ test_app.py
@@ -1,12 +1,48 @@
 import pytest
 import app as main
+from database import db
+from models import User
 
 
 @pytest.fixture
 def app():
     app = main.create_app()
     app.debug = True
-    return app.test_client()
+
+    # disabling CSRF protection for testing purposes
+    app.WTF_CSRF_ENABLED = False
+    client = app.test_client()
+    ctx = app.app_context()
+    ctx.push()
+
+    yield client # magic happens here
+
+    ctx.pop()
+
+
+@pytest.fixture
+def init_database():
+    # create db
+    db.create_all()
+
+    #  let's make two users
+    user1 = User(username='test1_user', phone='8675309')
+    user2 = User(username='test2_user', phone='1111111')
+
+    # set passwords
+    user1.set_password('8675309', 'wouldnt-you-like-to-know')
+    user2.set_password('1111111', 'deep-state-secrets')
+
+    # add to db
+    db.session.add(user1)
+    db.session.add(user2)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield db  # magic happens here
+
+    db.drop_all()
 
 
 def test_login_page(app):
@@ -19,7 +55,7 @@ def test_registration_page(app):
     assert res.status_code == 200
 
 
-def test_registration(app):
+def test_registration(app, init_database):
     username = "test_user"
     phone = "8675309"
     password = "top-secret-password"
@@ -28,13 +64,12 @@ def test_registration(app):
         phone=phone,
         password=password
     ))
-    print(res.data)
     assert res.status_code == 200
 
 
-def test_login(app):
+def test_login(app, init_database):
     username = "test_user"
-    phone = "8675309"
+    phone = "1111111"
     password = "top-secret-password"
     res = app.post("/login", data=dict(
         username=username,
