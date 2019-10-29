d11e6587c5fa1c4277c0b31811476127f7c02b2c
Brandon <b.mazey@nyu.edu>
2019-10-29 15:43:43 -0400

    hmmm these tests are passing when they shouldn't be :S

--- test_app.py
+++ test_app.py
@@ -9,11 +9,36 @@ def app():
     return app.test_client()
 
 
-def test_login(app):
+def test_login_page(app):
     res = app.get("/login")
     assert res.status_code == 200
 
 
-def test_registration(app):
+def test_registration_page(app):
     res = app.get("/register")
+    assert res.status_code == 200
+
+
+def test_registration(app):
+    username = "test_user"
+    phone = "8675309"
+    password = "top-secret-password"
+    res = app.post("/register", data=dict(
+        username=username,
+        phone=phone,
+        password=password
+    ))
+    print(res.data)
+    assert res.status_code == 200
+
+
+def test_login(app):
+    username = "test_user"
+    phone = "8675309"
+    password = "top-secret-password"
+    res = app.post("/login", data=dict(
+        username=username,
+        phone=phone,
+        password=password
+    ))
     assert res.status_code == 200
