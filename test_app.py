fd49bc5ce6d6d0620c9bfd16f75e4bc482e0e862
Brandon <b.mazey@nyu.edu>
2019-10-29 15:05:54 -0400

    alright! back this week and adding TESTS! pytest working and travis is set up ... now need to check tox

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import pytest
+import app as main
+
+
+@pytest.fixture
+def app():
+    app = main.create_app()
+    app.debug = True
+    return app.test_client()
+
+
+def test_login(app):
+    res = app.get("/login")
+    assert res.status_code == 200
