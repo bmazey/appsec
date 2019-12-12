eb07b88be98baea8cb9470dfe4e78c3b45dd347d
Brandon <b.mazey@nyu.edu>
2019-12-12 16:03:50 -0500

    adding docker secrets

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+from get_docker_secret import get_docker_secret
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     # secret key
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'wouldnt-you-like-to-know'
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'wouldnt-you-like-to-know'
+    SECRET_KEY = get_docker_secret('my_secret', default='oops very secret!')
+    print('secret key: ' + str(SECRET_KEY))
 
     # database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
